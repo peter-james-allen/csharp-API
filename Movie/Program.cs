570a674cee59351b3879e0ed8931e0272977c98a
Pete <peter_james_allen@hotmail.com>
2021-06-23 23:02:30 +0100

    created API call

--- Movie/Program.cs
+++ Movie/Program.cs
@@ -4,6 +4,8 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Net.Http.Formatting;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace Movie
 {
@@ -11,42 +13,70 @@ namespace Movie
     {
         static void Main(string[] args)
         {
-            Search("flubber");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Search for a Movie:");
+            string searchTerm = Console.ReadLine();
+            var searchResults = SearchMovies(searchTerm);
+            MovieSearchResults movieJson = JsonConvert.DeserializeObject<MovieSearchResults>(searchResults);
+            if (movieJson.Response)
+            {
+                foreach (Movie movie in movieJson.Search)
+                {
+                    Console.WriteLine($"Title: {movie.Title}\nYear: {movie.Year}\n");
+                    //Console.WriteLine($"Year: {movie.Year}");
+                }
+            }
+
+            //Movie jaws = new Movie(title: "Jaws", year: 1975, imdbID: "tt0073195", type: "movie", poster: "https://m.media-amazon.com/images/M/MV5BMmVmODY1MzEtYTMwZC00MzNhLWFkNDMtZjAwM2EwODUxZTA5XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg");
+
+            //string json = JsonConvert.SerializeObject(jaws);
+
+            //Movie readMovie = JsonConvert.DeserializeObject<Movie>(json);
         }
 
-        public class DataObject
-        {
-            public string Result { get; set; }
-        }
-
-        static void Search(string title)
+        static string SearchMovies(string title)
         {
             const string URL = "https://www.omdbapi.com/";
             const string apiKey = "&apikey=23bb5b7a";
 
-            HttpClient client = new HttpClient();
+            using var client = new HttpClient();
             client.BaseAddress = new Uri(URL);
-            string searchParams = "?" + title + apiKey;
+            string searchParams = "?s=" + title + apiKey;
 
-            // List data response.
-            HttpResponseMessage response = client.GetAsync(searchParams).Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
-            if (response.IsSuccessStatusCode)
-            {
-                Console.WriteLine(response.Content);
-                // Parse the response body..
-                var dataObjects = response.Content.ReadAsAsync<IEnumerable<DataObject>>().Result;  //Make sure to add a reference to System.Net.Http.Formatting.dll
-                foreach (var d in dataObjects)
-                {
-                    Console.WriteLine("{0}", d);
-                }
-            }
-            else
-            {
-                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
-            }
+            var content = client.GetStringAsync(URL + searchParams);
 
-            return;
+            return content.Result;
+        }
+    }
+
+    public class Movie
+    {
+        public string Title { get; }
+        public int Year { get; }
+        public string ImdbID { get; }
+        public string Type { get; }
+        public string Poster { get; }
+
+        public Movie(string title, int year, string imdbID, string type, string poster)
+        {
+            Title = title;
+            Year = year;
+            ImdbID = imdbID;
+            Type = type;
+            Poster = poster;
+        }
+    }
+
+    public class MovieSearchResults
+    {
+        public IList<Movie> Search { get; }
+        public string TotalResults { get; }
+        public bool Response { get; }
+
+        public MovieSearchResults(IList<Movie> search, string totalResults, bool response)
+        {
+            Search = search;
+            TotalResults = totalResults;
+            Response = response;
         }
     }
 }
